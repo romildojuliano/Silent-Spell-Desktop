c7b42fb16dbae8c7937f9796ad752a58c7fe383a
rjal <rjal@cin.ufpe.br>
2021-12-30 23:36:17 -0300

    Adicionados eventos para alternar entre jogos

--- Utils/Events.py
+++ Utils/Events.py
@@ -3,5 +3,7 @@ import pygame
 
 class EventType(Enum):
     START = pygame.event.custom_type()
+    START_RAIN = pygame.event.custom_type()
+    START_JOKENPO = pygame.event.custom_type()
     DAMAGE = pygame.event.custom_type()
     GAMEOVER = pygame.event.custom_type()
