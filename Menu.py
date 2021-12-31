980d7e7e1ff2328a92195591e14065cdd25bbf17
rjal <rjal@cin.ufpe.br>
2021-12-30 23:37:08 -0300

    Eventos para alternar entre jogos adicionados

--- Menu.py
+++ Menu.py
@@ -4,10 +4,10 @@ from Components.Button import Button
 from typing import List
 from Utils.Events import EventType
 
-
 class Menu():
     buttons: List[Button]
     screen: pygame.surface.Surface
+    dimensions: dict
 
     def __init__(self):
         pygame.init()
@@ -23,12 +23,28 @@ class Menu():
                           (200, 0, 0), (WIDTH//2, HEIGHT//2), (80, 60), True, [self.quit_game])
 
         self.buttons = [start_btn, exit_btn]
+        self.dimensions = {'width': WIDTH, 
+                           'height': HEIGHT}
 
     def start_game(self):
         pygame.event.post(pygame.event.Event(EventType.START.value))
+        
+    def choose_game(self):
+        self.screen = pygame.display.set_mode((self.dimensions['width'], self.dimensions['height']))
+        rain_button = Button(self.screen, 'Rain Game', 'arial', 20, (255, 255, 255),
+                           (200, 0, 0), (self.dimensions['width']//2, self.dimensions['height']//3), (80, 60), True, [self.start_rain_game])
+        jokenpo_button = Button(self.screen, 'Jokenpo', 'arial', 20, (255, 255, 255),
+                          (200, 0, 0), (self.dimensions['width']//2, self.dimensions['height']//2), (80, 60), True, [self.start_jokenpo])
+        self.buttons = [rain_button, jokenpo_button]
 
     def quit_game(self):
         pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+    def start_rain_game(self):
+        pygame.event.post(pygame.event.Event(EventType.START_RAIN.value))
+    
+    def start_jokenpo(self):
+        pygame.event.post(pygame.event.Event(EventType.START_JOKENPO.value))
 
     def __del__(self):
         for btn in self.buttons:
